7c81adf13e9687036eac3b076f8910652d6950a3
peti.kovacs7 <peti.kovacsik7@gmail.com>
2023-09-19 17:52:05 +0200

    errors fixed

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // int *b = new int[NELEMENTS];
-    // std::cout << '1-100 ertekek duplazasa'
-    // for (int i = 0;)
-    // {
-    //     b[i] = i * 2;
-    // }
-    // for (int i = 0; i; i++)
-    // {
-    //     std::cout << "Ertek:"
-    // }    
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
+    for (int i = 0;i<N_ELEMENTS;i++)
+    {
+        b[i] = i * 2;
+    }
+    for (int i = 0; i<N_ELEMENTS; i++)
+    {
+        std::cout << "Ertek: "<<i<<std::endl;
+    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    // int atlag;
-    // for (int i = 0; i < N_ELEMENTS, i++)
-    // {
-    //     atlag += b[i]
-    // }
-    // atlag /= N_ELEMENTS;
-    // std::cout << "Atlag: " << atlag << std::endl;
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
+    {
+        atlag += b[i];
+    }
+    atlag /= N_ELEMENTS;
+    std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
